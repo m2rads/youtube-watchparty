21300afd35a4b0552eb1a54aa11cdf3ab524da8f
m2rads <argo.mohrad@gmail.com>
2022-08-20 11:39:14 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  let player1, player2;
+  let player1State, player2State;
+  import Youtube from "./lib/Youtube.svelte";
+</script>
+
+<hr />
+<Youtube
+  videoId="M7lc1UVf-VE"
+  on:PlayerStateChangeString={({ detail }) => (player1State = detail)}
+  bind:this={player1}
+/><br />
+<button on:click={() => player1.play()}> play v1 </button><br />
+Player 1 State: {player1State}
