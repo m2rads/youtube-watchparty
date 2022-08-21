b935968d2bdfd89abe064618089e6359b5adda36
m2rads <argo.mohrad@gmail.com>
2022-08-20 21:14:20 -0700

    Updat App: add Home view

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,5 @@
 <script>
-  let player1, player2;
-  let player1State, player2State;
-  import Youtube from "./lib/Youtube.svelte";
+  import Home from "./components/Home.svelte";
 </script>
 
-<hr />
-<Youtube
-  videoId="M7lc1UVf-VE"
-  on:PlayerStateChangeString={({ detail }) => (player1State = detail)}
-  bind:this={player1}
-/><br />
-<button on:click={() => player1.play()}> play v1 </button><br />
-Player 1 State: {player1State}
+<Home />
