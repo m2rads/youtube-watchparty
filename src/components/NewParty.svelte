c8dd045da8cc7b83af113e88e7e2839b64ba46dc
m2rads <argo.mohrad@gmail.com>
2022-08-24 17:30:47 -0700

    add youtube player

--- src/components/NewParty.svelte
+++ src/components/NewParty.svelte
@@ -1,16 +1,70 @@
 <script>
-  let player1, player2;
-  let player1State, player2State;
+  // import "./timeline.css";
+  let player1;
+  let player1State;
   import Youtube from "../lib/Youtube.svelte";
+  import { currentTimeLine } from "../store/store";
+  let playStatus = true;
+  let currentTime = 0;
+
+  currentTimeLine.subscribe((value) => {
+    currentTime = value;
+  });
 </script>
 
-<p>New Party</p>
+<hr />
 
-<!-- <hr /> -->
-<!-- <Youtube
-    videoId="M7lc1UVf-VE"
-    on:PlayerStateChangeString={({ detail }) => (player1State = detail)}
-    bind:this={player1}
-  /><br />
-  <button on:click={() => player1.play()}> play v1 </button><br />
-  Player 1 State: {player1State} -->
+<Youtube
+  videoId="M7lc1UVf-VE"
+  on:PlayerStateChangeString={({ detail }) => (player1State = detail)}
+  bind:this={player1}
+/><br />
+<!-- <button on:click={() => player1.play()}>. </button> -->
+<div class="video-controller">
+  <button
+    on:click={() => {
+      player1.pause();
+      playStatus = true;
+    }}
+    class="button pause">⏮</button
+  >
+  {#if playStatus}
+    <button
+      on:click={() => {
+        player1.play();
+        playStatus = false;
+      }}
+      class="button play"
+    >
+      ▶️</button
+    >
+  {:else}
+    <button
+      on:click={() => {
+        player1.pause();
+        playStatus = true;
+      }}
+      class="button pause">⏸</button
+    >
+  {/if}
+  <button
+    on:click={() => {
+      player1.pause();
+      playStatus = true;
+    }}
+    class="button pause">⏭</button
+  >
+  <input
+    type="range"
+    value={currentTime}
+    on:change={(e) => player1.seekTo(e.target.value / 100, true)}
+  />
+</div>
+
+<!-- Player 1 State: {player1State} -->
+<style>
+  input[type="range"] {
+    /* padding: 0 10px; */
+    width: 80%;
+  }
+</style>
